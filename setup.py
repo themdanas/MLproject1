c4eb804984da0e8986ee525c50036bdc60d74971
themdanas <mdanasakbari@gmail.com>
2025-08-02 14:22:49 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+    
+setup(
+    name='ML-Project',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Md Anas',
+    author_email='anasthemd@gmail.com',
+    install_requires=get_requirements('requirements.txt')   
+)
